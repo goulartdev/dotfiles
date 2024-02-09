33b20f579de99cc5496951e952813985f3ef893f
Djonathan Goulart <contact@djonathan.dev>
2024-02-08 23:11:47 -0300

    remove qmk

--- hosts/laptop/users/djonathan/default.nix
+++ hosts/laptop/users/djonathan/default.nix
@@ -54,7 +54,6 @@
     wl-clipboard
     rofi-wayland
     gh
-    qmk
     podman-tui
     lazygit      # A simple terminal UI for git commands https://github.com/jesseduffield/lazygit
     delta        # A syntax-highlighting pager for git, diff, and grep output https://github.com/dandavison/delta
