f15853b76d68e7df5c3fc1b98a3df421e1dd7122
Djonathan Goulart <contact@djonathan.dev>
2024-02-10 13:59:06 -0300

    add devShells

--- flake.nix
+++ flake.nix
@@ -4,6 +4,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
+    # waiting for https://github.com/NixOS/nixpkgs/pull/287016 to be merged
     vlc.url = "github:NixOS/nixpkgs/75457994b8d28e951075d0fa8ec6605ba9585778";
 
     agenix = {
@@ -28,20 +29,15 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    auto-cpufreq = {
-      url = "github:adnanhodzic/auto-cpufreq";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
   };
 
   outputs = { nixpkgs, ... }@inputs:
   let
+    forAllSystems = nixpkgs.lib.genAttrs [ "x86_64-linux" ];
     overlays = {
       nixpkgs.overlays = [
         inputs.rust-overlay.overlays.default
         (final: prev: {
-          disko = inputs.disko.packages.x86_64-linux.default;
           vlc = inputs.vlc.legacyPackages.x86_64-linux.vlc;
           agenix = inputs.agenix.packages.x86_64-linux.default;
         })
@@ -50,27 +46,43 @@
     };
   in
   {
-    nixosConfigurations.laptop = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        ./hosts/laptop/configuration.nix
-        inputs.home-manager.nixosModules.default 
-        inputs.agenix.nixosModules.default
-        inputs.disko.nixosModules.default
-        inputs.impermanence.nixosModules.impermanence
-        inputs.auto-cpufreq.nixosModules.default
-        overlays
-      ];
+    # overlays = import ./nix/overlays { inherit inputs; };
+
+    nixosConfigurations = {
+      laptop = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux" 
+          modules = [
+          ./hosts/laptop/configuration.nix
+            inputs.home-manager.nixosModules.default 
+            inputs.agenix.nixosModules.default
+            inputs.disko.nixosModules.default
+            inputs.impermanence.nixosModules.impermanence
+            overlays
+          ];
+      };
+      installer = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux"
+          modules = [
+          ./hosts/installer/configuration.nix
+            inputs.agenix.nixosModules.default
+            inputs.disko.nixosModules.default
+            overlays
+          ];
+      };
     };
 
-    nixosConfigurations.installer = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        ./hosts/installer/configuration.nix
-        inputs.agenix.nixosModules.default
-        inputs.disko.nixosModules.default
-        overlays
-      ];
-    };
+    devShells = forAllSystems (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            gcc # needed for installing some nvim plugins
+            disko
+            agenix
+          ];
+        };
+      }
+    )
   };
 }
