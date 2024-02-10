e705b9b6830536cc46fd6657eb337a750430d9b2
Djonathan Goulart <contact@djonathan.dev>
2024-02-10 12:38:34 -0300

    remove rofi

--- hosts/laptop/users/djonathan/default.nix
+++ hosts/laptop/users/djonathan/default.nix
@@ -52,7 +52,6 @@
     zellij
     neovim
     wl-clipboard
-    rofi-wayland
     gh
     podman-tui
     lazygit      # A simple terminal UI for git commands https://github.com/jesseduffield/lazygit
