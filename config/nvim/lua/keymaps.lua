b13bed268a80038ec056e14a3feb0f4e25c2b4f2
Djonathan Goulart <contact@djonathan.dev>
2024-02-15 21:45:47 -0300

    vim bindings update

--- config/nvim/lua/keymaps.lua
+++ config/nvim/lua/keymaps.lua
@@ -3,12 +3,15 @@
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = 'Scroll halfscreen down and center current line' })
 vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = 'Scroll halfscreen aup nd center current line' })
+vim.keymap.set("n", "n", "nzzzv", { desc = 'Next search result and center line' })
+vim.keymap.set("n", "N", "Nzzzv", { desc = 'Prior search result and center line' })
 
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
+-- paste and delete without copying to register
 vim.keymap.set("x", "<leader>p", [["_dP]])
-vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
-vim.keymap.set("n", "<leader>Y", [["+Y]])
+vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
+
 --
 -- Remap for dealing with word wrap
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
@@ -21,8 +24,8 @@ vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open float
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
 
 -- [[ Highlight on yank ]]
--- See `:help vim.highlight.on_yank()`
 local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
+
 vim.api.nvim_create_autocmd('TextYankPost', {
   callback = function()
     vim.highlight.on_yank()
