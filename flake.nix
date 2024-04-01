4d5ef599633cfc23b48031041154204c95491d89
Djonathan Goulart <contact@djonathan.dev>
2024-04-01 00:29:32 -0300

    address CVE-2024-3094

--- flake.nix
+++ flake.nix
@@ -4,6 +4,9 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
+    xz-rollback.url =
+      "github:NixOS/nixpkgs/699ac3316c2618ff1e4cd176a35df2a19b5f226c";
+
     agenix = {
       url = "github:ryantm/agenix";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -29,50 +32,50 @@
   };
 
   outputs = { nixpkgs, ... }@inputs:
-  let
-    forAllSystems = nixpkgs.lib.genAttrs [ "x86_64-linux" ];
-    overlays = {
-      nixpkgs.overlays = [
-        inputs.rust-overlay.overlays.default
-        (import ./pkgs)
-      ];
-    };
-  in
-  {
-    nixosConfigurations = {
-      laptop = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          ./hosts/laptop/configuration.nix
-          inputs.home-manager.nixosModules.default 
-          inputs.agenix.nixosModules.default
-          inputs.disko.nixosModules.default
-          inputs.impermanence.nixosModules.impermanence
-          overlays
+    let
+      forAllSystems = nixpkgs.lib.genAttrs [ "x86_64-linux" ];
+      overlays = {
+        nixpkgs.overlays = [
+          inputs.rust-overlay.overlays.default
+          (final: prev: {
+            xz = inputs.xz-rollback.legacyPackages.x86_64-linux.xz;
+          })
+          (import ./pkgs)
         ];
       };
-      installer = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          ./hosts/installer/configuration.nix
-          inputs.agenix.nixosModules.default
-          inputs.disko.nixosModules.default
-          overlays
-        ];
-      };
-    };
-
-    devShells = forAllSystems (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in {
-        default = pkgs.mkShell {
-          nativeBuildInputs = with pkgs; [
-            disko
-            inputs.agenix.packages.${system}.default
+    in {
+      nixosConfigurations = {
+        laptop = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [
+            ./hosts/laptop/configuration.nix
+            inputs.home-manager.nixosModules.default
+            inputs.agenix.nixosModules.default
+            inputs.disko.nixosModules.default
+            inputs.impermanence.nixosModules.impermanence
+            overlays
           ];
         };
-      }
-    );
-  };
+        installer = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [
+            ./hosts/installer/configuration.nix
+            inputs.agenix.nixosModules.default
+            inputs.disko.nixosModules.default
+            overlays
+          ];
+        };
+      };
+
+      devShells = forAllSystems (system:
+        let pkgs = nixpkgs.legacyPackages.${system};
+        in {
+          default = pkgs.mkShell {
+            nativeBuildInputs = with pkgs; [
+              disko
+              inputs.agenix.packages.${system}.default
+            ];
+          };
+        });
+    };
 }
