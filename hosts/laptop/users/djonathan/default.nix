4cef0c07a7b2ff41f8a414033d4f40cc340b67c2
Djonathan Goulart <contact@djonathan.dev>
2025-12-14 11:25:09 -0300

    zsh: change fast-syntax-highlighting plugin dir name

--- hosts/laptop/users/djonathan/default.nix
+++ hosts/laptop/users/djonathan/default.nix
@@ -141,7 +141,7 @@
   };
 
   xdg.dataFile = {
-    "zsh/plugins/zsh-fast-syntax-highlighting".source =
+    "zsh/plugins/fast-syntax-highlighting".source =
       "${pkgs.zsh-fast-syntax-highlighting}/share/zsh/plugins/fast-syntax-highlighting";
     "zsh/plugins/zsh-autosuggestions".source = "${pkgs.zsh-autosuggestions}/share/zsh-autosuggestions";
     "zsh/plugins/zsh-vi-mode".source = "${pkgs.zsh-vi-mode}/share/zsh-vi-mode";
