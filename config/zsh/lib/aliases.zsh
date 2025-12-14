2c032fa6c8b332bd96b7f4c0d9d19ebd1da3e155
Djonathan Goulart <contact@djonathan.dev>
2025-12-14 11:24:03 -0300

    zsh: remove wifi-fix alias

--- config/zsh/lib/aliases.zsh
+++ config/zsh/lib/aliases.zsh
@@ -34,7 +34,6 @@ alias ff="fzf --preview 'bat --style=numbers --color=always {}'"
 alias sudo='doas '
 alias myip='curl ipinfo.io/ip'
 alias nvidia-settings='nvidia-settings --config=$XDG_CONFIG_HOME/nvidia/settings'
-alias wifi-fix='sudo modprobe -r ath10k_pci && sudo modprobe ath10k_pci'
 
 alias develop='nix develop -c zsh'
 
