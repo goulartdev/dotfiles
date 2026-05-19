634376c821794343b5ff1d2ed38a397c2a703614
Djonathan Goulart <contact@djonathan.dev>
2026-05-18 23:07:35 -0300

    fix typo

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -4,12 +4,14 @@ local unmap = vim.keymap.del
 map("n", "<C-c>", "<cmd>bw<cr>", { desc = "Close current buffer" })
 
 map("n", "<C-d>", "<C-d>zz", { desc = "Scroll halfscreen down and center current line" })
-map("n", "<C-u>", "<C-u>zz", { desc = "Scroll halfscreen aup nd center current line" })
+map("n", "<C-u>", "<C-u>zz", { desc = "Scroll halfscreen up and center current line" })
 
 map("n", "<leader>rw", [[:%s/\<<C-r><C-w>\>//gI<Left><Left><Left>]], { desc = "Replace current Word" })
 
 map("n", "<leader><space>", LazyVim.pick("files", { hidden = true }), { desc = "Find Files (Root Dir)" })
-map("n", "<leader>s.", function() Snacks.picker.recent({ filter = { cwd = true }}) end, {desc = "Recent (cwd)"})
+map("n", "<leader>s.", function()
+  Snacks.picker.recent({ filter = { cwd = true } })
+end, { desc = "Recent (cwd)" })
 
 unmap("n", "<leader>fT")
 unmap("n", "<leader>ft")
